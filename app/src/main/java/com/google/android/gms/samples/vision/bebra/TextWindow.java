99037e57001729d836b28809252cdcea3e60bf10
yarik <yarik.resanov@gmail.com>
2022-05-12 15:49:03 +0300

    added new buttons and functions

--- app/src/main/java/com/google/android/gms/samples/vision/bebra/TextWindow.java
+++ app/src/main/java/com/google/android/gms/samples/vision/bebra/TextWindow.java
@@ -1,8 +1,14 @@
 package com.google.android.gms.samples.vision.bebra;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class TextWindow extends AppCompatActivity {
     public static String text;
@@ -13,5 +19,17 @@ public class TextWindow extends AppCompatActivity {
         setContentView(R.layout.activity_text_window);
         TextView some = findViewById(R.id.textView);
         some.setText(text);
+    }
+    public void onClickCopyButton(View view){
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("Copied Text", text);
+        clipboard.setPrimaryClip(clip);
+        Toast toast = Toast.makeText(TextWindow.this,
+                "Text copied to clipboard", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+    public void onClickExitButton(View view){
+        Intent i = new Intent(TextWindow.this, OcrCaptureActivity.class);
+        startActivity(i);
     }
 }
